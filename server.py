0bde69cf9d5bbedec1f4fb93a0cfac5a7c966dd6
Rohit <rohitjain18005@gmail.com>
2022-02-06 22:19:33 +0530

    added out.json return

--- server.py
+++ server.py
@@ -1,20 +1,81 @@
-import re
+import imp
+import mercantile
+import requests
 from flask import Flask
 from flask import request
 from infer_mapbox_input import execute_script
+import shutil as sh
+import os
+import json
 
 app = Flask(__name__)
 
+def getImageMapBox(latitude, longitude):
+    MAPBOX_PUBLIC_KEY = "pk.eyJ1Ijoicm9oaXRqYWluMDAiLCJhIjoiY2t6OHRpYXM3MTd1ZDJwcW51M2xsamxkZyJ9.y2T27Pnd-g2fdCSZljvJlg"
+    got_pre_disaster_image = False
+    got_post_disaster_image = False
+
+    delta = 0.02
+    zoom = 15
+
+    min_latitude = latitude - delta
+    max_latitude = latitude + delta
+    min_longitude = longitude - delta
+    max_longitude = longitude + delta
+
+    upper = mercantile.tile(min_longitude, max_latitude, zoom)
+    lower = mercantile.tile(max_longitude, min_latitude, zoom)
+
+    x_tile_range = [upper.x, lower.x]
+    y_tile_range = [upper.y, lower.y]
+    print("x_tile_range = ", x_tile_range)
+    print("y_tile_range = ", y_tile_range)
+
+    x = upper.x
+    y = upper.y
+
+    r = requests.get(
+        f"https://api.mapbox.com/v4/mapbox.satellite/{zoom}/{x}/{y}@2x.png?access_token={MAPBOX_PUBLIC_KEY}", 
+        stream=True
+    )
+    if r.status_code == 200:
+        got_pre_disaster_image = True
+        with open(f"./pre_disaster.png", "wb") as f:
+            r.raw.decode_content = True
+            sh.copyfileobj(r.raw, f)
+    else:
+        print("[ERROR]: Pre Disaster Response: ", r)
+    
+    # Post Disaster
+    r = requests.get(
+        f"https://api.mapbox.com/v4/jackkwok.digitalglobe_harvey_3020132_tif/{zoom}/{x}/{y}@2x.png?access_token={MAPBOX_PUBLIC_KEY}", 
+        stream=True
+    )
+    if r.status_code == 200:
+        got_post_disaster_image = True
+        with open(f"./inference/post_disaster.png", "wb") as f:
+            r.raw.decode_content = True
+            sh.copyfileobj(r.raw, f)
+    else:
+        print("[ERROR]: Post Disaster Response: ", r)
+                
+    if got_pre_disaster_image and not got_post_disaster_image:
+        raise ValueError("Incompatible Latitude and Longitude. Try a different location")
+    
 @app.route('/', methods=['GET'])
 def hello():
-    lat = request.args.get('lat')
-    long = request.args.get('lon')
+    lat = float(request.args.get('lat'))
+    long = float(request.args.get('lon'))
     if not lat or not long:
         return "No lat long provided"
     print('Starting execution')
-    tid = execute_script(lat, long)
+    # tid = execute_script(lat, long)
+    # outJson, tid = get_outJson(getImageMapBox(lat, long))
+    getImageMapBox(lat, long)
+    a = os.system("python infer_custom_input.py -input pre_disaster.png -gsd 0.5 -model_id 2 -output out.json")
     print('execution complete')
-    return { "tid" : tid}
+    outJson = json.load(open('out.json'))
+    return json.dumps(outJson)
 
 
 if __name__ == "__main__":
